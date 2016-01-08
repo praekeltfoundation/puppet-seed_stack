a0f7b67f244576040d6c2359763fd90efa835829
Jamie Hewland <jhewland@gmail.com>
2016-01-08 12:56:28 +0200

    Add test infrastructure

---
+++ spec/classes/worker_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'seed_stack::worker' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile }
+    end
+  end
+end
