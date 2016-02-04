8c39c1ee7d16d1a0641af739457295b65f276d5c
Jamie Hewland <jhewland@gmail.com>
2016-02-04 14:26:17 +0200

    Make the nginx router listen address/port and server_name configurable

--- manifests/router.pp
+++ manifests/router.pp
@@ -2,12 +2,30 @@
 #
 # Defines a dynamically-cofigured Nginx router for internal apps. Apps must
 # define a 'location' Marathon label to be accessible through Nginx.
-class seed_stack::router {
+#
+# === Parameters
+#
+# [*listen_address*]
+#   The address that Nginx should listen on when serving requests.
+#
+# [*listen_port*]
+#   The port that Nginx should listen on when serving requests.
+#
+# [*domain*]
+#   The domain that Nginx should serve for routing.
+class seed_stack::router (
+  $listen_address = '0.0.0.0',
+  $listen_port    = 80,
+  $domain         = 'servicehost',
+) {
+  validate_ip_address($listen_address)
+  validate_integer($listen_port, 65535, 0)
+
   include seed_stack::template_nginx
 
   # Configure Nginx to route to upstream services
   file { '/etc/consul-template/nginx-services.ctmpl':
-    source => 'puppet:///modules/seed_stack/nginx-services.ctmpl',
+    content => template('seed_stack/nginx-services.ctmpl.erb'),
   }
   ~>
   consul_template::watch { 'nginx-services':
