6164b45a74ba4cca64802267314be0f09e5938aa
Jamie Hewland <jhewland@gmail.com>
2016-02-08 12:54:26 +0200

    Rework parameter tests

--- spec/classes/controller_spec.rb
+++ spec/classes/controller_spec.rb
@@ -4,33 +4,28 @@ describe 'seed_stack::controller' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts
+        facts.merge({:concat_basedir => '/tmp/puppetconcat'})
       end
 
-      let(:params) do
-        {
-          :controller_addresses => ['192.168.0.2'],
-          :address => '192.168.0.2',
-        }
+      describe 'compiles when controller_addresses and address are specified' do
+        let(:params) do
+          {
+            :controller_addresses => ['192.168.0.2'],
+            :address => '192.168.0.2',
+          }
+        end
+        it { should compile }
       end
 
-      it { should compile }
-    end
-
-    context 'controller_addresses is required' do
-      let(:params) do
-        { :address => '192.168.0.2', }
+      describe 'controller_addresses must be specified' do
+        let(:params) { { :address => '192.168.0.2', } }
+        it { should compile.and_raise_error(/Must pass controller_addresses/) }
       end
 
-      it { should compile.and_raise_error(/Must pass controller_addresses/) }
-    end
-
-    context 'address is required' do
-      let(:params) do
-        { :controller_addresses => ['192.168.0.2'], }
+      describe 'address must be specified' do
+        let(:params) { { :controller_addresses => ['192.168.0.2'], } }
+        it { should compile.and_raise_error(/Must pass address/) }
       end
-
-      it { should compile.and_raise_error(/Must pass address/) }
     end
   end
 end
--- spec/classes/worker_spec.rb
+++ spec/classes/worker_spec.rb
@@ -7,30 +7,25 @@ describe 'seed_stack::worker' do
         facts.merge({:concat_basedir => '/tmp/puppetconcat'})
       end
 
-      let(:params) do
-        {
-          :controller_addresses => ['192.168.0.2'],
-          :address => '192.168.0.3',
-        }
+      describe 'compiles when controller_addresses and address are specified' do
+        let(:params) do
+          {
+            :controller_addresses => ['192.168.0.2'],
+            :address => '192.168.0.3',
+          }
+        end
+        it { should compile }
       end
 
-      it { should compile }
-    end
-
-    context 'controller_addresses is required' do
-      let(:params) do
-        { :address => '192.168.0.2', }
+      describe 'controller_addresses must be specified' do
+        let(:params) { { :address => '192.168.0.2', } }
+        it { should compile.and_raise_error(/Must pass controller_addresses/) }
       end
 
-      it { should compile.and_raise_error(/Must pass controller_addresses/) }
-    end
-
-    context 'address is required' do
-      let(:params) do
-        { :controller_addresses => ['192.168.0.2'], }
+      describe 'address must be specified' do
+        let(:params) { { :controller_addresses => ['192.168.0.2'], } }
+        it { should compile.and_raise_error(/Must pass address/) }
       end
-
-      it { should compile.and_raise_error(/Must pass address/) }
     end
   end
 end
