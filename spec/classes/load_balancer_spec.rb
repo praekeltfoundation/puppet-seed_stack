f5f88a8500d81d2cd5c48a59172dccde590bdcb8
Jamie Hewland <jhewland@gmail.com>
2016-02-12 11:51:44 +0200

    Fix style problems in load_balancer_spec.rb

--- spec/classes/load_balancer_spec.rb
+++ spec/classes/load_balancer_spec.rb
@@ -4,7 +4,7 @@ describe 'seed_stack::load_balancer' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-        facts.merge({:concat_basedir => '/tmp/puppetconcat'})
+        facts.merge(:concat_basedir => '/tmp/puppetconcat')
       end
 
       it { is_expected.to compile }
