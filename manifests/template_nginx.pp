0f89475ffeb61ec0c26318239d0a1a27ddb4f41c
Colin Alston <colin.alston@gmail.com>
2016-03-09 13:16:54 +0200

    default to true

--- manifests/template_nginx.pp
+++ manifests/template_nginx.pp
@@ -20,7 +20,7 @@
 # [*consul_address*]
 #   The address for the Consul agent for Consul Template to connect to.
 class seed_stack::template_nginx (
-  $nginx_manage            = undef,
+  $nginx_manage            = true,
   $nginx_package           = 'nginx-light',
   $nginx_package_ensure    = $seed_stack::cluster_params::nginx_ensure,
   $nginx_service_ensure    = 'running',
