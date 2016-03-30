1e91cfac1d4e26885190fa84bf1d1db8b0d18417
Jeremy Thurgood <jeremy@praekelt.com>
2016-03-30 15:10:35 +0200

    Use new xylem plugin for managing mc2 container.

--- manifests/mc2.pp
+++ manifests/mc2.pp
@@ -7,36 +7,13 @@ class seed_stack::mc2(
   $hub_domain,
   $marathon_host = 'marathon.service.consul',
 ) {
-
-  unless defined(Package['curl']) {
-    package { 'curl': ensure => 'installed' }
-  }
-
-  file { '/usr/local/bin/manage-marathon-group.sh':
-    source  => 'puppet:///modules/seed_stack/manage-marathon-group.sh',
-    mode    => '0755',
-    require => Package['curl'],
-  }
-
   file { '/etc/marathon-apps': ensure => 'directory' }
   ->
   file { '/etc/marathon-apps/mc2.marathon.json':
     content => template('seed_stack/mc2.marathon.json.erb'),
   }
 
-  $mc2_marathon_cmd = join([
-    '/usr/local/bin/manage-marathon-group.sh',
-    '/etc/marathon-apps/mc2.marathon.json',
-    $marathon_host,
-  ], ' ')
-
-  # This is unconditional because Marathon does nothing when the group
-  # definition is unchanged.
-  exec { 'mc2 marathon app':
-    command => $mc2_marathon_cmd,
-    require => [
-      File['/usr/local/bin/manage-marathon-group.sh'],
-      File['/etc/marathon-apps/mc2.marathon.json'],
-    ],
+  class { 'xylem::config::marathon_sync':
+    group_json_files => ['/etc/marathon-apps/mc2.marathon.json'],
   }
 }
